8c08474d8aa84490a4e0fc5edbf1a3937ed1fd1a
Tehsein <tehseinf@gmail.com>
2021-05-19 16:32:02 +0530

    Added output box

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter.constants import END
 
 root=tk.Tk()
+root.title("ROCK-PAPER-SCISSORS")
 
 # setting the windows size
 root.geometry("600x400")
@@ -15,16 +17,23 @@ inp_var=tk.StringVar()
 def sub():
     global score
     score=score_var.get()
-    print("The maximum score is : " + score)
+    Output.insert(END,"The maximum score is : " + score)
     score_var.set("")
     
 
 def submit():
     global inp
     inp=inp_var.get()
-    print("User chose : " + inp)
+    Output.insert(END,"\nUser chose : " + inp)
     inp_var.set("")
 	
+
+
+Output = tk.Text(root, height = 25, 
+              width = 25, 
+              bg = "light cyan")
+
+
 	
 
 score_label = tk.Label(root, text = 'Enter the Maximum Score', font=('calibre',10, 'bold'))
@@ -49,6 +58,7 @@ inp_label.grid(row=1,column=0)
 inp_entry.grid(row=1,column=1)
 sub_btn2.grid(row=0,column=3)
 sub_btn1.grid(row=1,column=3)
+Output.grid(row=2,column=1)
 
 
 root.mainloop()
